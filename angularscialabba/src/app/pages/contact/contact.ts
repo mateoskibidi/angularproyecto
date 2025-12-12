3c0114a5ab58eb717e3592a8652f91c3c0d85e56
mateo scialabba <mateoscialabba7@gmail.com>
2025-12-12 16:59:13 -0300

    holaa

--- angularscialabba/src/app/pages/contact/contact.ts
+++ angularscialabba/src/app/pages/contact/contact.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormGroupName } from '@angular/forms';
-import { from } from 'rxjs';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
 
+  miformulario = new FormGroup({
+    email: new FormControl(``, [Validators.required, Validators.email]),
+    nombre: new FormControl(``, Validators.required),
+    apellido: new FormControl(``, Validators.required,)
+  })
+
+  colecciondeusers: User[] = []
+
+  enviardatos() {
+    if (this.miformulario.valid) {
+      const user: User = {
+        email: this.miformulario.value.email!,
+        nombre: this.miformulario.value.nombre!,
+        apellido: this.miformulario.value.apellido!
+      }
+
+      this.colecciondeusers.push(user)
+
+      alert("se agrego un nuevo usuario")
+    }
+
+
+    /*  this.miformulario = miformulario;
+ 
+     const datos = this.miformulario.value
+ 
+     alert(`email: ${datos.email}\n nombre: ${datos.nombre}\n apellido: ${datos.apellido} `
+ 
+     ) */
+  }
 
 
 }
